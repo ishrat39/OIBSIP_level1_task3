a48f3fc7f877c285f653e080b382b62357d0fa23
Saquib Hasnain <125003788+Saquib9852github@users.noreply.github.com>
2023-04-01 14:02:20 +0530

    initial comit

---
+++ ishrat.js
@@ -0,0 +1,12 @@
+let uservalue = document.getElementById("input");
+let btn = document.getElementById("btn");
+let showValue = document.getElementById("Value");
+
+btn.onclick = function () {
+  if (uservalue.value) {
+    let data = ((uservalue.value - 32) * 5) / 9;
+    showValue.innerHTML = data + "°C";
+  } else {
+    alert("Enter value");
+  }
+};
